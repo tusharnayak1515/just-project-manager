53a59a114aaa9887ef2e1e695b4a6094ac47f500
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2022-07-31 17:01:16 +0530

    added media queries in spinnermodal

--- components/SpinnerModal.jsx
+++ components/SpinnerModal.jsx
@@ -1,14 +1,19 @@
+import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
-import React from 'react';
 import ReactDom from 'react-dom';
-// import Spinner from '../public/static/images/processloading.gif';
 import Spinner from '../public/static/images/loading8.svg';
 
 import styles from '../styles/spinnermodal.module.css';
 
 const SpinnerModal = () => {
+  const topRef = useRef();
+
+  useEffect(()=> {
+    topRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
   return ReactDom.createPortal(
-    <div className={styles.overlay}>
+    <div className={styles.overlay} ref={topRef}>
       <div className={styles.modal}>
         <Image src={Spinner} alt="Loading Spinner" />
       </div>
