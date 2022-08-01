a0bd0c530176ab66d334fd96897e768703a7b2ef
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2022-08-01 16:04:33 +0530

    added back button media queries in 404

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-// import invalidBanner from '../public/static/images/invalid6.jpg';
+import { useRouter } from 'next/router';
 import invalidBanner from '../public/static/images/notFound.gif';
 
 import styles from '../styles/404.module.css';
 
 const Not_Found = () => {
+  const router = useRouter();
+
   const onRightCLick = (e)=> {
     e.preventDefault();
+  }
+
+  const goBack = (e)=> {
+    e.preventDefault();
+    router.replace("/");
   }
 
   return (
@@ -21,9 +28,9 @@ const Not_Found = () => {
           />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        {/* <Image src={invalidBanner} rel="Invalid Page gif" height="730px" width="1536px" /> */}
-        <Image src={invalidBanner} alt="Invalid Page gif" height="400px" width="500px" onContextMenu={onRightCLick} />
+        <Image src={invalidBanner} alt="Invalid Page gif" onContextMenu={onRightCLick} />
         <h1>PAGE NOT FOUND</h1>
+        <button onClick={goBack}>Go Back</button>
     </div>
   )
 }
